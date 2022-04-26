4009171722fd2efc3eec76d3e629ebcad4ea6a87
Phuc Tran <xuanphuc1209@gmai.com>
2022-04-26 18:29:50 -0500

    rotate the food in place

--- GreedySnake/GreedySnake/Assets/Scripts/Food.cs
+++ GreedySnake/GreedySnake/Assets/Scripts/Food.cs
@@ -13,7 +13,7 @@ public class Food : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.Rotate(0, 50 * Time.deltaTime, 0);
 	}
 	void OnTriggerEnter(Collider other){
 		if(isTrigger) return;
